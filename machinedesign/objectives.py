9305035693102b802dbf8313b863b474e5900f6f
Mehdi Cherti <mehdicherti@gmail.com>
2016-12-18 17:32:22 +0100

    add axis categorial crossentropy

--- machinedesign/objectives.py
+++ machinedesign/objectives.py
@@ -11,21 +11,38 @@ from keras import objectives
 __all__ = [
     "mean_squared_error",
     "get_loss",
-    "custom_objectives"
+    "custom_objectives",
+    "axis_categorical_crossentropy"
 ]
 
 def mean_squared_error(y_true, y_pred):
     """mean squared error (mean over all axes)"""
     return K.mean(K.square(y_pred - y_true))
 
+def axis_categorical_crossentropy(y_true, y_pred, axis=1):
+    yt = y_true.argmax(axis=axis) # supposed to be onehot in the axis 'axis'
+    yt = yt.flatten()#convert it to a vector
+    perm = list(range(y_pred.ndim))
+    # permute 'axis' and the first axis
+    perm[axis], perm[0] = perm[0], perm[axis]
+    perm = tuple(perm)
+    ypr = y_pred.transpose(perm)
+    ypr = ypr.reshape((ypr.shape[0], -1))
+    ypr = ypr.T
+    return K.categorical_crossentropy(ypr, yt)
+
 custom_objectives = {
-    'mean_squared_error': mean_squared_error
+    'axis_categorical_crossentropy': axis_categorical_crossentropy
 }
 
 def get_loss(name, objectives=objectives):
-    try:
-        func = custom_objectives[name]
-    except KeyError:
-        return getattr(objectives, name)
+    if isinstance(name, dict):
+        #TODO do that
+        raise NotImplementedError()
     else:
-        return func
+        try:
+            func = custom_objectives[name]
+        except KeyError:
+            return getattr(objectives, name)
+        else:
+            return func
---
+++ machinedesign/tests/test_objectives.py
@@ -0,0 +1,31 @@
+import numpy as np
+import keras.backend as K
+
+from machinedesign.objectives import axis_categorical_crossentropy
+
+def test_axis_categorical_crossentropy():
+    X = K.placeholder(shape=(None, 2, 10, 10))
+    Y = K.placeholder(shape=(None, 2, 10, 10))
+    pred = K.function([X, Y], axis_categorical_crossentropy(X, Y, axis=1))
+    np.random.seed(42)
+    nb = 100
+
+    label = np.random.randint(0, 1 , size=(nb * 100))
+    y = np.zeros((nb * 100, 2))
+    y[label==0, 0] = 1
+    y[label==1, 1] = 1
+
+    x = np.random.uniform(0, 1, size=(nb * 100, 2))
+    x[label==0, 0] = 0.9
+    x[label==1, 0] = 0.1
+    x[:, 1] = 1 - x[:, 0]
+
+    x = x.reshape((nb, 10, 10, 2))
+    x = x.transpose((0, 3, 1, 2))
+
+    y = y.reshape((nb, 10, 10, 2))
+    y = y.transpose((0, 3, 1, 2))
+
+    l = pred([y, x])
+    assert l.shape == (nb*10*10,)
+    assert np.allclose(l, -np.log(0.9))

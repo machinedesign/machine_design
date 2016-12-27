c319a36e63851c0a0a0b08ac6ee545bc0eefa764
Mehdi Cherti <mehdicherti@gmail.com>
2016-12-27 21:25:27 +0100

    add feature space and objectness loss

--- machinedesign/objectives.py
+++ machinedesign/objectives.py
@@ -5,8 +5,11 @@ the functions here are used as objective functions (loss), rather
 than metrics, so they are written in terms of keras backend (symbolic),
 rather than numpy.
 """
+from functools import partial
+
 from keras import backend as K
 from keras import objectives
+from keras.models import load_model
 
 __all__ = [
     "mean_squared_error",
@@ -16,7 +19,7 @@ __all__ = [
 ]
 
 def mean_squared_error(y_true, y_pred):
-    """mean squared error (mean over all axes)"""
+    """mean squared error (mean over all axes except the first)"""
     y_true = y_true.flatten(2)
     y_pred = y_pred.flatten(2)
     return K.mean(K.square(y_pred - y_true), axis=1)
@@ -41,7 +44,7 @@ def axis_categorical_crossentropy(y_true, y_pred, axis=1):
     Returns
     -------
 
-    scalar
+    vector
     """
     yt = y_true.argmax(axis=axis) # supposed to be onehot in the axis 'axis'
     yt = yt.flatten()#convert it to a vector
@@ -52,34 +55,98 @@ def axis_categorical_crossentropy(y_true, y_pred, axis=1):
     ypr = y_pred.transpose(perm)
     ypr = ypr.reshape((ypr.shape[0], -1))
     ypr = ypr.T
-    return K.categorical_crossentropy(ypr, yt).mean()
+    return K.categorical_crossentropy(ypr, yt)
+
+def feature_space_mean_squared_error(y_true, y_pred, model_filename=None, layer=None):
+    """mean squared error on a feature space defined by a model"""
+    assert model_filename is not None, 'Please specify `model_filename` in the parameters of the loss'
+    assert layer is not None, 'Please specifiy `layer` in the parameters of the loss'
+    model = load_model(model_filename)
+    return mean_squared_error(model(y_true), model(y_pred))
+
+def objectness(y_true, y_pred, model_filename=None):
+    """
+    Compute the objectness [1] score based on a classifier in `model_filename`.
+    this loss does not use `y_true`, it only uses the classifier and `y_pred`.
+
+    References
+    ----------
+
+    [1] Improved techniques for training gans
+        T.Salimans, I.Goodfellow, W.Zaremba, V.Cheung, A.Radford, X.Chen
+        Advances in Neural Information Processing Systems, 2226-2234
+    """
+    assert model_filename is not None, 'Please specify `model_filename` in the parameters of the loss'
+    model = load_model(model_filename)
+    return _compute_objectness(model(y_pred))
+
+def _compute_objectness(probas):
+    pr = probas
+    marginal = pr.mean(axis=0)
+    score = pr * K.log(pr / marginal)
+    score = score.sum(axis=1)
+    return score
+
+def loss_sum(y_true, y_pred, terms=[]):
+    """
+    a loss which is a weighted sum of several losses
+
+    parameters
+    ----------
+
+    terms: list of dict
+        each dict should have a key 'loss' and a key 'coef'.
+        'loss' can be either a dict or a str. It defines the loss.
+        See `get_loss` to know when to use a str and when to use a dict.
+        'coef' is a float defining the importance given to the loss.
+    """
+    assert len(terms), 'Please specify `terms` in the parameters of the loss'
+    total = 0
+    for loss_def in terms:
+        total += get_loss(loss_def['loss'])(y_true, y_pred) * loss_def['coef']
+    return total
 
 custom_objectives = {
-    'axis_categorical_crossentropy': axis_categorical_crossentropy
+    'axis_categorical_crossentropy': axis_categorical_crossentropy,
+    'feature_space_mean_squared_error': feature_space_mean_squared_error,
+    'objectness': objectness,
+    'sum': loss_sum
 }
 
-def get_loss(name, objectives=objectives):
+def get_loss(loss, objectives=objectives):
     """
-    get loss function given a `name`.
-    If the name is one of the keys defined in `custom_objectives`,
+    get loss function given a `loss`.
+    `loss` can either be a string, and in that case, it will correspond
+    the name of the loss. In case the loss has itself parameters, a dict
+    can be passed instead of a str, thus `loss` can be a dict.
+    If `loss` is a dict, then it should have a key `name` and a keys `params`.
+    On both cases, if the name is one of the keys defined in `custom_objectives`,
     then use it. if not, search in `objectives`.
     if it does not find in either of these, it will return an exception.
 
     Parameters
     ----------
 
-    name : str
-        name of the loss function
+    loss : str or dict
+        loss to use, see the description above to know when to use
+        str and when to use dict.
     objectives : object
-        object (e.g module) containing objectives (by default its `keras.objectives`)
+        object (e.g module) containing objectives (by default it is `keras.objectives`)
     """
-    if isinstance(name, dict):
-        #TODO do that (handle params for loss function)
-        raise NotImplementedError()
-    else:
+    if isinstance(loss, dict):
+        name = loss['name']
+        params = loss['params']
         try:
             func = custom_objectives[name]
         except KeyError:
-            return getattr(objectives, name)
+            func = getattr(objectives, name)
+        func = partial(func, **params)
+        return func
+    # assume loss is a str
+    else:
+        try:
+            func = custom_objectives[loss]
+        except KeyError:
+            return getattr(objectives, loss)
         else:
             return func

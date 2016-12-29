d6092d5def484fcbbd4fb20d6eb7f1892c057b6b
Mehdi Cherti <mehdicherti@gmail.com>
2016-12-29 18:32:35 +0100

    add plug and play generative networks

---
+++ machinedesign/ppgn/interface.py
@@ -0,0 +1,80 @@
+"""
+Implementation of the generation procedure of Plug and Play generative networks.
+See paper at <https://arxiv.org/pdf/1612.00005v1.pdf>.
+"""
+import numpy as np
+import os
+
+from keras.models import load_model
+import keras.backend as K
+
+from ..common import custom_objects
+from ..common import mkdir_path
+
+def train(params):
+    # no need for training ppgn
+    pass
+
+def generate(params):
+    nb_samples = params['nb_samples']
+    nb_iter = params['nb_iter']
+    folder = params['folder']
+    feature_space_autoencoder = params['feature_space_autoencoder']
+    classifier_filename = params['classifier']
+    genetor_filename = params['generator']
+    epsilon1 = params['epsilon1']
+    epsilon2 = params['epsilon2']
+    epsilon3 = params['epsilon3']
+
+    class_indices = params['class_indices']
+    seed = params['seed']
+
+    rng = np.random.RandomState(seed)
+
+    classifier = load_model(classifier_filename, custom_objects=custom_objects)
+    generator = load_model(genetor_filename, custom_objects=custom_objects)
+    feature_space_autoencoder = load_model(feature_space_autoencoder, custom_objects=custom_objects)
+
+    # follow paper notation (see eq. 11 of paper)
+    C = classifier
+    G = generator
+    H = feature_space_autoencoder
+
+    e1 = epsilon1
+    e2 = epsilon2
+    e3 = epsilon3
+
+    # check compositionality constraints
+    msg = "'feature_space_autoencoder' expects input_shape == output_shape, input_shape is {} while output_shape is {}"
+    msg = msg.format(H.input_shape, H.output_shape)
+    assert H.input_shape == H.output_shape, msg
+
+    msg = "'generator' input_shape ({}) must be 'feature_space_autoencoder' output_shape ({}), they are different"
+    msg = msg.format(G.input_shape, feature_space_autoencoder.output_shape)
+    assert G.input_shape == feature_space_autoencoder.output_shape, msg
+
+    msg = "'generator' output_shape ({}) must be 'classifier' input_shape ({}), they are different"
+    msg = msg.format(G.output_shape, feature_space_autoencoder.input_shape)
+    assert G.output_shape == classifier.input_shape, msg
+
+    h = K.placeholder(feature_space_autoencoder.input_shape)
+
+    # get the two needed terms (see eq. 11 of paper)
+
+    h_rec = H(h) - h # diff between reconstruction and current feature space, shape : e.g (nb_examples, hid)
+    x = G(h) # generated objects from feature space, shape : e.g (nb_examples, c, h, w)
+    y = K.log(C(x)) # predicted probabilities of the classifier, shape : e.g (nb_examples, nb_classes)
+    dc_dh =  K.gradients(y[:, class_indices].sum(), h) # feature space gradients, shape : e.g (nb_examples, hid)
+
+    delta_h = e1 * h_rec + e2 * dc_dh
+    get_delta_h = K.function([h], delta_h)
+    get_generated = K.function([h], x)
+
+    hid = rng.normal(0, 1, size=(nb_iter, nb_samples) + H.input_shape[1:])
+    generated = np.zeros(size=(nb_iter, nb_samples) + G.input_shape[1:])
+    for i in range(1, nb_iter):
+        hid[i] = hid[i - 1] + get_delta_h([hid[i - 1]]) + rng.normal(0, e3, size=hid[i - 1].shape)
+        generated[i] = get_generated(hid[i])
+    mkdir_path(folder)
+    filename = os.path.join(folder, 'generated.npz')
+    np.savez_compressed(filename, full=generated, generated=generated[-1])
